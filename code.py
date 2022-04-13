3d3eb6e6b71577256b90ef9545dbc3e49fa35073
PouletRaPHOO <raphael.herzlich@icloud.com>
2022-04-13 12:32:14 +0200

    MapDisplay

--- code.py
+++ code.py
@@ -3,18 +3,55 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
+def drawMap2D(map) :
+    size = 64
+    for y in range(len(map)) :
+        for x in range(len(map[y])):
+            if map[y][x]==1 :
+                glColor3f(1.0,1.0,1.0)
+            else :
+                glColor3f(1.0,0.0,3.0)
+            x0 = 500-(x*size)
+            y0 = 500-(y*size)
+            glBegin(GL_QUADS)
+            glVertex2i(x0,y0)
+            glVertex2i(x0,y0+size)
+            glVertex2i(x0+size,y0+size)
+            glVertex2i(x0+size,y0)
+            glEnd()
+
+
+def iterate() :
+    glViewport(0,0,500,500)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0.0,500,0.0,500,0.0,1.0)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+def square() :
+    glBegin(GL_QUADS)
+    glVertex2f(100,100)
+    glVertex2f(100,200)
+    glVertex2f(200,200)
+    glVertex2f(200,100)
+    glEnd()
+
 class Grid:
-    SIZE = 8
-    grille = [
-            [1,1,1,1,1,1,1,1],
-            [1,0,0,0,0,0,0,1],
-            [1,0,0,0,0,0,0,1],
-            [1,0,0,0,0,0,0,1],
-            [1,0,0,0,0,0,0,1],
-            [1,0,0,0,0,0,0,1],
-            [1,0,0,0,0,0,0,1],
-            [1,1,1,1,1,1,1,1]
-        ]
+    def __init__(self) :
+        self.SIZE = 8
+        self.grille = [
+                [1,1,1,1,1,1,1,1],
+                [1,0,0,0,0,0,0,1],
+                [1,0,0,0,1,0,0,1],
+                [1,0,0,0,0,0,0,1],
+                [1,0,0,0,0,0,0,1],
+                [1,0,0,1,0,0,0,1],
+                [1,0,0,0,0,0,0,1],
+                [1,1,1,1,1,1,1,1]
+            ]
+
+grille = Grid()
 
 class Inputs:
     def keyboard(key, x, y):
@@ -25,12 +62,13 @@ class Inputs:
 def display():
     # Clear the color and depth buffers
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
-    Draw.draw_square(3, 3, 7, 6)
     # ... render stuff in here ...
     # It will go to an off-screen frame buffer.
-     
-    drawMap2D(map)
+    glLoadIdentity()
+    iterate()
+    glColor3f(1.0,0.0,3.0)
+    #square()
+    drawMap2D(grille.grille)
     # Copy the off-screen buffer to the screen.
     glutSwapBuffers()
 
@@ -40,32 +78,18 @@ glutInit(sys.argv)
 # So is creating an index-mode window.)
 glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
 
+glutInitWindowSize(500,500)
+glutInitWindowPosition(0,0)
+
 # Create a window, setting its title
 glutCreateWindow('Doomineur-3D')
 
 # Set the display callback.  You can set other callbacks for keyboard and
 # mouse events.
 glutDisplayFunc(display)
+glutIdleFunc(display)
 glutKeyboardFunc(Inputs.keyboard)
 
 # Run the GLUT main loop until the user closes the window.
 glutMainLoop()
 
-
-
-def drawMap2D(map) :
-    for y in range(len(map)) :
-        for x in range(len(map[y])):
-            if x ==1 :
-                glColor3f(1,1,1)
-            else :
-                glColors3f(0,0,0)
-            x0 = x*64
-            y0 = y*64
-            glBegin(GL_QUADS)
-            glVertex(x0,y0)
-            glVertex(x0,y0+64)
-            glVertex(x0+64,y0)
-            glVertex(x0+64,y0+64)
-            glEnd()
-            
